c4ec34ebd10dd3b1511709209874de7111b50c24
allisonleigh808 <allisonleigh808@gmail.com>
2020-07-27 20:40:11 +0000

    started in models.py

--- suicideprevent/models.py
+++ suicideprevent/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Mood(models.Model):
+    happy_face = models.CharField(max_length=255, null=True, blank=True)
+    sad_face= models.CharField(max_length=255, null=True, blank=True)
+    angry_face = models.CharField(max_length=255, null=True, blank=True)
+    cry_face = models.CharField(max_length=255, null=True, blank=True)
+    neutral_face = models.CharField(max_length=255, null=True, blank=True)
+    annoyed_face = models.CharField(max_length=255, null=True, blank=True)
+    disgust_face = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+         return f"{self.neutral_face}"
+         
+        
