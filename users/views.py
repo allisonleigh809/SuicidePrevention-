10620745454dff7fbed3d4ce259454f0c440d51b
allisonleigh808 <allisonleigh808@gmail.com>
2020-07-29 22:22:45 +0000

    created a superuser and changed things in user models and views. py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import User
+from .models import Moods
 
 # Create your views here.
+def update_profile(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+    user.save()
