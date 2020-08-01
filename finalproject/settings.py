58ac4e0ebced2a7b99818bffa6683c364b372b35
allisonleigh808 <allisonleigh808@gmail.com>
2020-08-01 20:24:13 +0000

    got my admin to work and going to start putting things in.

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'suicideprevent',
-    'users',
+    
 
     # Third-party
     'debug_toolbar',
@@ -117,6 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#  AUTH_USER_MODEL = [
+#     'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
